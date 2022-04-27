3bd40681647aac15f04c6ee7b0188c20c2f428e7
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-04-27 18:07:54 +0700

    Format code StaffInfoComponent.js

--- reactcomponent/src/components/StaffInfoComponent.js
+++ reactcomponent/src/components/StaffInfoComponent.js
@@ -6,37 +6,37 @@ class StaffInfo extends Component {
     if (this.props.staffInfo != null) {
       return (
         <div className="row m-2">
-            <div className="col-lg-12 col-md-12 col-sm-12 col-12">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-12">
             <div className="card mb-3">
-            <div className="row no-gutters">
-              <div className="col-md-4">
-                <img src="logo192.png" className="card-img" alt="..." />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">
-                    Họ và tên: {this.props.staffInfo.name}
-                  </h5>
-                  <p className="card-text">
-                    Ngày sinh:{" "}
-                    {moment(this.props.staffInfo.doB).format("DD/MM/YYYY")}
-                  </p>
-                  <p className="card-text">
-                    Ngày vào công ty: {moment(this.props.staffInfo.startDate).format("DD/MM/YYYY")}
-                  </p>
-                  <p className="card-text">Phòng ban: {this.props.department}</p>
-                  <p className="card-text">
-                    Số ngày nghỉ còn lại: {this.props.staffInfo.annualLeave}
-                  </p>
-                  <p className="card-text">
-                    Số ngày đã làm thêm: {this.props.staffInfo.overTime}
-                  </p>
+              <div className="row no-gutters">
+                <div className="col-md-4">
+                  <img src="logo192.png" className="card-img" alt="..." />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      Họ và tên: {this.props.staffInfo.name}
+                    </h5>
+                    <p className="card-text">
+                      Ngày sinh: {moment(this.props.staffInfo.doB).format("DD/MM/YYYY")}
+                    </p>
+                    <p className="card-text">
+                      Ngày vào công ty: {moment(this.props.staffInfo.startDate).format("DD/MM/YYYY")}
+                    </p>
+                    <p className="card-text">
+                      Phòng ban: {this.props.department}
+                    </p>
+                    <p className="card-text">
+                      Số ngày nghỉ còn lại: {this.props.staffInfo.annualLeave}
+                    </p>
+                    <p className="card-text">
+                      Số ngày đã làm thêm: {this.props.staffInfo.overTime}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-            </div>
-        
         </div>
       );
     } else {
