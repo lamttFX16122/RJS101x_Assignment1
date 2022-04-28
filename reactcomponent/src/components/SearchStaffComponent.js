f1ef684a44d260b839cdf20f57bcf16e54c8dc72
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-04-28 14:15:57 +0700

    Add Filter Component SearchStaffComponent.js

--- reactcomponent/src/components/SearchStaffComponent.js
+++ reactcomponent/src/components/SearchStaffComponent.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 
 class SearchStaff extends Component {
     onHandleSearch=(e)=>{ this.props.onReceiveStaffName(e.target.value);
-        console.log(e.target.value);
+        // console.log(e.target.value);
     }
   render() {
 
       return (
         <div className="row">
         <div className="form-group">
-          <label>Search</label>
+          <label>Lọc Nhân viên theo tên</label>
           <input type="text" className="form-control" onChange={this.onHandleSearch} name="txtName" placeholder="Tên nhân viên"/>
         </div>
       </div>
--- reactcomponent/src/components/StaffListComponent.js
+++ reactcomponent/src/components/StaffListComponent.js
@@ -1,46 +1,60 @@
 import React, { Component } from "react";
 
 class StaffList extends Component {
-    staffClick=(value)=>{
-        this.props.onReceiveStaff(value);
-    }
+  staffClick = (value) => {
+    this.props.onReceiveStaff(value);
+  };
   render() {
-    let list = this.props.listStaff.map((value) => {
-      // var stsring = "Trương Thanh Lâm";
-      // console.log(stsring.toUpperCase().indexOf(name.toUpperCase()));
-      if(this.props.filterName==null || this.props.filterName=="")
-      {
+    let list = "";
+    let count = 0;
+    if (this.props.filterName == null || this.props.filterName == "") {
+      count++;
+      list = this.props.listStaff.map((value) => {
         return (
           <div key={value.id} className={this.props.column}>
-            <div className="card text-white bg-info cart-list" onClick={()=>this.staffClick(value)}>
+            <div
+              className="card text-white bg-info cart-list"
+              onClick={() => this.staffClick(value)}
+            >
               <div className="card-body">
-                <p className="card-text">
-                  {value.name}
-                </p>
+                <p className="card-text">{value.name}</p>
               </div>
             </div>
           </div>
         );
-      }else
-      {
-        if(value.name.toUpperCase().indexOf(this.props.filterName.toUpperCase())>=0)
-        {
+      });
+    } else {
+     
+      list = this.props.listStaff.map((value) => {
+        if (
+          value.name
+            .toUpperCase()
+            .indexOf(this.props.filterName.toUpperCase()) >= 0
+        ) {
+          count++;
           return (
             <div key={value.id} className={this.props.column}>
-              <div className="card text-white bg-info cart-list" onClick={()=>this.staffClick(value)}>
+              <div
+                className="card text-white bg-info cart-list"
+                onClick={() => this.staffClick(value)}
+              >
                 <div className="card-body">
-                  <p className="card-text">
-                    {value.name}
-                  </p>
+                  <p className="card-text">{value.name}</p>
                 </div>
               </div>
             </div>
           );
         }
-      }
-   
-    });
-    return <div className="row m-2">{list}</div>;
+      });
+    }
+    if (count != 0) {
+      return <div className="row m-2">{list}</div>;
+    } else
+      return (
+        <div className="row m-2">
+          <p>Không tìm nhân viên có tên <b>{this.props.filterName}</b></p>
+        </div>
+      );
   }
 }
 
