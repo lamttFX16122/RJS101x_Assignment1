dc6458bd80aa0a036f5b4af5dcfd9981ce151760
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-04-28 11:12:52 +0700

    Format code SetColumnComponent.js

--- reactcomponent/src/components/SetColumnComponent.js
+++ reactcomponent/src/components/SetColumnComponent.js
@@ -1,72 +1,65 @@
 import React, { Component } from "react";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
+// import DropdownButton from "react-bootstrap/DropdownButton";
+// import Dropdown from "react-bootstrap/Dropdown";
 
 class SetColumn extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      column: 0
-    }
+    this.state = {
+      column: 0,
+    };
   }
-  handleCheck(e){
-    if(e.currentTarget.dataset.id=="0")
-    {
-      this.props.setCulumn("col-lg-4 col-md-6 col-sm-6 col-12")
-    }
-    else if(e.currentTarget.dataset.id=="1")
-    {
-      this.props.setCulumn("col-12")
-    }
-    else if(e.currentTarget.dataset.id=="2")
-    {
-      this.props.setCulumn("col-6")
-    }
-    else if(e.currentTarget.dataset.id=="3")
-    {
-      this.props.setCulumn("col-4 small")
-    }
-    
-  }
-  HandleChange=event=>{
-    let target=event.target;
-    let name=target.name;
-    let value=target.value;
+  // ===========================================Code For Dropdown==================================
+  // handleCheck(e) {
+  //   if (e.currentTarget.dataset.id == "0") {
+  //     this.props.setCulumn("col-lg-4 col-md-6 col-sm-6 col-12");
+  //   } else if (e.currentTarget.dataset.id == "1") {
+  //     this.props.setCulumn("col-12");
+  //   } else if (e.currentTarget.dataset.id == "2") {
+  //     this.props.setCulumn("col-6");
+  //   } else if (e.currentTarget.dataset.id == "3") {
+  //     this.props.setCulumn("col-4 small");
+  //   }
+  // }
+  HandleChange = (event) => {
+    let target = event.target;
+    let name = target.name;
+    let value = target.value;
     this.setState({
-      column:value
-    })
+      column: value,
+    });
     switch (value) {
       case "1":
         this.props.setCulumn("col-12");
         break;
-        case "2":
-          this.props.setCulumn("col-6");    
-          break;
-          case "3":
-            this.props.setCulumn("col-4");
-            break;
-          
+      case "2":
+        this.props.setCulumn("col-6");
+        break;
+      case "3":
+        this.props.setCulumn("col-4");
+        break;
       default:
-        this.props.setCulumn("col-lg-4 col-md-6 col-sm-6 col-12");    
+        this.props.setCulumn("col-lg-4 col-md-6 col-sm-6 col-12");
         break;
     }
-  }
+  };
   render() {
-      return (
-        <div className="row slt-columnchange">
-          <div className="form-group">
-            {/* <label>Số cột hiển thị</label> */}
-            <select className="form-control" name="column" 
+    return (
+      <div className="row slt-columnchange">
+        <div className="form-group">
+          {/* <label>Số cột hiển thị</label> */}
+          <select
+            className="form-control"
+            name="column"
             value={this.state.column}
             onChange={this.HandleChange}
-            >
-              <option value={0}>Mặc định</option>
-              <option value={1}>1 cột</option>
-              <option value={2}>2 cột</option>
-              <option value={3}>3 cột</option>
-            </select>
-          </div>
-
+          >
+            <option value={0}>Mặc định</option>
+            <option value={1}>1 cột</option>
+            <option value={2}>2 cột</option>
+            <option value={3}>3 cột</option>
+          </select>
+        </div>
 
         {/* <DropdownButton id="dropdown-basic-button" className="drop" title="Số cột hiển thị">
           <Dropdown.Item onClick={this.handleCheck.bind(this)}  data-id="0" href="#/action-1">Mặc định</Dropdown.Item>
@@ -75,7 +68,7 @@ class SetColumn extends Component {
           <Dropdown.Item onClick={this.handleCheck.bind(this)} data-id="3" href="#/action-3">3 cột</Dropdown.Item>
         </DropdownButton> */}
       </div>
-      );
+    );
   }
 }
 export default SetColumn;
