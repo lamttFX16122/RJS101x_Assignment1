740fe0e0b590ad29b9141d8e771d9c1b25099341
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-04-28 11:01:19 +0700

    Change Dropdown to Selection setColumnComponent.js

--- reactcomponent/src/components/SetColumnComponent.js
+++ reactcomponent/src/components/SetColumnComponent.js
@@ -3,6 +3,12 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 
 class SetColumn extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      column: 0
+    }
+  }
   handleCheck(e){
     if(e.currentTarget.dataset.id=="0")
     {
@@ -22,15 +28,52 @@ class SetColumn extends Component {
     }
     
   }
+  HandleChange=event=>{
+    let target=event.target;
+    let name=target.name;
+    let value=target.value;
+    this.setState({
+      column:value
+    })
+    switch (value) {
+      case "1":
+        this.props.setCulumn("col-12");
+        break;
+        case "2":
+          this.props.setCulumn("col-6");    
+          break;
+          case "3":
+            this.props.setCulumn("col-4");
+            break;
+          
+      default:
+        this.props.setCulumn("col-lg-4 col-md-6 col-sm-6 col-12");    
+        break;
+    }
+  }
   render() {
       return (
-        <div className="col-12 drop-col">
-        <DropdownButton id="dropdown-basic-button" className="drop" title="Số cột hiển thị">
+        <div className="row">
+          <div className="form-group">
+            {/* <label>Số cột hiển thị</label> */}
+            <select className="form-control" name="column" 
+            value={this.state.column}
+            onChange={this.HandleChange}
+            >
+              <option value={0}>Mặc định</option>
+              <option value={1}>1 cột</option>
+              <option value={2}>2 cột</option>
+              <option value={3}>3 cột</option>
+            </select>
+          </div>
+
+
+        {/* <DropdownButton id="dropdown-basic-button" className="drop" title="Số cột hiển thị">
           <Dropdown.Item onClick={this.handleCheck.bind(this)}  data-id="0" href="#/action-1">Mặc định</Dropdown.Item>
           <Dropdown.Item onClick={this.handleCheck.bind(this)} data-id="1" href="#/action-2">1 cột</Dropdown.Item>
           <Dropdown.Item onClick={this.handleCheck.bind(this)} data-id="2" href="#/action-3">2 cột</Dropdown.Item>
           <Dropdown.Item onClick={this.handleCheck.bind(this)} data-id="3" href="#/action-3">3 cột</Dropdown.Item>
-        </DropdownButton>
+        </DropdownButton> */}
       </div>
       );
   }
